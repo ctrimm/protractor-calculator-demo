5dc22e42aa9b385211de32024c1b732d4a7d825d
Cory Trimm <cory.trimm@gmail.com>
2014-09-18 08:33:35 -0400

    this checks the history of past calculations

--- spec.js
+++ spec.js
@@ -4,31 +4,23 @@ describe('angularjs homepage', function() {
   var secondNumber = element(by.model('second'));
   var goButton = element(by.id('gobutton'));
   var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b){
+  	firstNumber.sendKeys(a);
+  	secondNumber.sendKeys(b);
+  	goButton.click();
+  }
 
   beforeEach(function() {
   	browser.get('http://juliemr.github.io/protractor-demo/');
   });
 
-  it('should have a title', function() {
-    expect(browser.getTitle()).toEqual('Super Calculator');
+  it('should have a history', function(){
+  	add(1, 2);
+  	add(3, 4);
+
+  	expect(history.last().getText()).toContain('1 + 2');
+  	expect(history.first().getText()).toContain('foo'); //This is incorrect
   });
-
-
-  it('should add one and two', function(){
-  	firstNumber.sendKeys(1);
-  	secondNumber.sendKeys(2);
-
-  	goButton.click();
-
-  	expect(latestResult.getText()).toEqual('3');
-  });
-
-  it('should add four and six', function(){
-  	element(by.model('first')).sendKeys(4);
-  	element(by.model('second')).sendKeys(6);
-
-  	goButton.click();
-
-  	expect(latestResult.getText()).toEqual('10');
-  })
 });
